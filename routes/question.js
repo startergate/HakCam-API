8c4b17f3c8a584aa2dbb7d54e26bff5559efcf39
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-08-06 06:58:56 +0900

    .

--- routes/question.js
+++ routes/question.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Express' });
+router.get('/:video', (req, res, next) => {
+    res.send();
+});
+
+router.get('/:video/:time', (req, res, next) => {
+    res.send();
 });
 
 module.exports = router;
