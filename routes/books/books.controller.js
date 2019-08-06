9d90f4f3c70e54d01305e420fe597b9115c9dcd4
ParkJongHyo <s18052@gsm.hs.kr>
2019-08-06 20:31:49 +0900

    url했는데 수레기댓음
    주석달아버릴거임

--- routes/books/books.controller.js
+++ routes/books/books.controller.js
@@ -2,12 +2,13 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: true, //디버깅목적..
+        headless: true, 
+        timeout:0,
         defaultViewport: null
     });
     const page = await browser.newPage();
  
-    await page.goto('http://book.gen.go.kr/r/reading/search/schoolCodeSetting.jsp?schoolCode=2353', {});
+    await page.goto('http://book.gen.go.kr/r/reading/search/schoolCodeSetting.jsp?schoolCode=2353', {timeout:0});
     await page.type('#searchCon2', 'C#');       // "C#"쪽을 다른 변수이름으로 바꿀날이 오긴 할까
     await page.click('[class=searchBtn]');
     await page.waitFor('.result');
@@ -15,7 +16,10 @@ const puppeteer = require('puppeteer');
     const result = await page.$$('.result tbody tr');
     for (let item of result) {
         let title = await item.$eval('a', node => node.innerText)
-        console.log(title);
+        /*let htmlw = await item.$eval('a', node => node.outerHTML)
+        let strArray = htmlw.split("'");
+        htmlw = 'http://book.gen.go.kr/r/reading/search/schoolBookDetail.jsp?controlNo='+strArray[1];*/
+        console.log(title/*, htmlw*/);
     }
  
     await browser.close();
